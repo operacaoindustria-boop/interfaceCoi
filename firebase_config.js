84185630350a65746b8dec24013b86c3fcf2e948
operacaoindustria-boop <operacaoindustria-boop@users.noreply.github.com>
2025-11-09 21:38:10 +0000

    deploy: 9b73e7b7d8992dd249231b4614003c28523abac2

---
+++ firebase_config.js
@@ -0,0 +1,12 @@
+﻿// ATENÇÃO: ESTE ARQUIVO SERÁ PREENCHIDO PELO GITHUB ACTIONS.
+// ELE CONTÉM SUAS CHAVES REAIS DO PROJETO integracaocoi.
+export const FIREBASE_CONFIG = {
+    apiKey: "", 
+    authDomain: "integracaocoi.firebaseapp.com",
+    projectId: "integracaocoi",
+    storageBucket: "integracaocoi.firebasestorage.app",
+    messagingSenderId: "177203745489",
+    appId: "1:177203745489:web:b86bbc4e96830e06032bf0"
+};
+
+export const APP_ID_FALLBACK = 'integracaocoi';
