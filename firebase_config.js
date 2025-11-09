d2a977cc5cc69de4ce769d389170a19e8cf89509
Seu Nome <seu@email.com>
2025-11-08 22:14:43 -0400

    Arquivos finais e automacao do GitHub Actions

--- firebase_config.js
+++ firebase_config.js
@@ -1,12 +1,12 @@
 ﻿// ATENÇÃO: ESTE ARQUIVO SERÁ PREENCHIDO PELO GITHUB ACTIONS.
-// MANTENHA OS PLACEHOLDERS ABAIXO, EXCETO SE VOCÊ QUISER FAZER CONFIGURAÇÃO MANUAL.
+// ELE CONTÉM SUAS CHAVES REAIS DO PROJETO integracaocoi.
 export const FIREBASE_CONFIG = {
-    apiKey: "##API_KEY##", 
-    authDomain: "##AUTH_DOMAIN##",
-    projectId: "##PROJECT_ID##",
-    storageBucket: "##STORAGE_BUCKET##",
-    messagingSenderId: "##MESSAGING_SENDER_ID##",
-    appId: "##APP_ID##"
+    apiKey: "AIzaSyB_ZscXHOoNP1Xkm-C6WLqhwEFcTsBk3GQ", 
+    authDomain: "integracaocoi.firebaseapp.com",
+    projectId: "integracaocoi",
+    storageBucket: "integracaocoi.firebasestorage.app",
+    messagingSenderId: "177203745489",
+    appId: "1:177203745489:web:b86bbc4e96830e06032bf0"
 };
 
-export const APP_ID_FALLBACK = 'github-pages-app-id';
+export const APP_ID_FALLBACK = 'integracaocoi';
